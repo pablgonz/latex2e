e8a4455c2ca88d1baf1ca4da2565959deb76545e
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-06-02 09:31:04 +0100

    Add core template code to the format
    This is an initial import of the xtemplate code
    stripping out anything not 100% agreed. To add
    definitely is a way to copy templates: like
    \DeclareRestrictedTemplate but without the
    restriction part.

---
+++ base/config-lttemplate.lua
@@ -0,0 +1,9 @@
+-- Tests for ltcmd (from latex3/l3packages/xtemplate)
+
+checkengines = {"etex","xetex","luatex"}
+checksearch  = false
+testfiledir  = "testfiles-lttemplate"
+
+testsuppdir = "../support"
+
+checkruns = 1
